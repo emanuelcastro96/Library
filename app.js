2e6c84563f5e2bc269d4b7a0a716aebac06c1780
EmanuelCastro <emanuelcastro96@gmail.com>
2020-10-05 18:19:55 -0300

    Tercer commit: agregado log de prueba para rama en app.js

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ sql.connect(config).catch((err) => {
 
 console.log('Prueba');
 
+console.log('Rama prueba');
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/jquery/dist')));
